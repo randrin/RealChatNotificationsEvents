05dede681aaddf66ac72487f9c60946876323a38
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2020-04-12 15:06:11 +0200

    Create Fakers Data and migrate with php artisan db:seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Model\Category;
+use App\Model\Like;
+use App\Model\Question;
+use App\Model\Reply;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(User::class, 20)->create();
+        factory(Category::class, 10)->create();
+        factory(Question::class, 8)->create();
+        factory(Reply::class, 50)->create()->each(function($reply) {
+            return $reply->likes()->save(factory(Like::class)->make());
+        });
     }
 }
